a387df26de29c0d354086e06ac08443d321adeae
John Cheney <71104613+johncheney@users.noreply.github.com>
2021-02-22 14:38:33 +0900

    Update RabbitsandReoccurenceRelations.py

--- RabbitsandReoccurenceRelations.py
+++ RabbitsandReoccurenceRelations.py
@@ -1,11 +1,12 @@
 
-n =
-k = 
-
-def rabbitcounter(): 
-  for i in range n:
-    answer = k*(n-2) + (n-１）
-    print(answer)
+def rabbitcounter(n,k): 
+  for i in range(n):
+    answer = k*(n-2) + (n-1)
+    print(answer) 
+    
+    
+    
+rabbitcounter(5,3)
     
     
 
