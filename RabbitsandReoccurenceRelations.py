f1b4e5e647c97be3c34277114bd42ce7adc4fd39
John Cheney <71104613+johncheney@users.noreply.github.com>
2021-03-03 13:24:19 +0900

    I think this works..?

--- RabbitsandReoccurenceRelations.py
+++ RabbitsandReoccurenceRelations.py
@@ -1,18 +1,18 @@
 
 def rabbitcounter(n,k):  # Describe the fxｎ
     p = 1
-    o = 0 
+    o = 1 
     pairs = [1,1,]
-    for i in range(2, n+1):        # for loop to loop through 
+    for i in range(2, n):        # for loop to loop through 
         o = o + p*k 
         p = o - p*k
         pairs.append(o+p)
         
-        print('parents =' ,p)
-        print('offspring =' ,o)
-        print('pairs =' ,pairs)
+        #print('parents =' ,p)
+    print('offspring =' ,o)
+        #print('pairs =' ,pairs)
         
-        print('  ')
+        #print('  ')
        # print('pairs' = pairs)
 
 
