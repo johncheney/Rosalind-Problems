ecaa6d71efabae10cc15c42ecd3bebd699eddbf7
John Cheney <71104613+johncheney@users.noreply.github.com>
2021-03-03 10:31:28 +0900

    Update RabbitsandReoccurenceRelations.py

--- RabbitsandReoccurenceRelations.py
+++ RabbitsandReoccurenceRelations.py
@@ -1,23 +1,22 @@
 
 
+def rabbitcounter(n,k):  # Describe the fxｎ
+    p = 1
+    o = 0 
+    for i in range(n):        # for loop to loop through 
+        pairs=o+p
+        #next_gen=
+        o = o + p*k 
+        p = o  
+        
+        print('parents =' ,p)
+        print('offspring =' ,o)
+        print('pairs =' ,pairs)
+        
+        print('  ')
+       # print('pairs' = pairs)
 
-
-
-pairs = [1, 1,]             # initalize a list to add to  
-
-def rabbitcounter(n,k):                        # Describe the fxｎ
-    for i in range(n-1):        # for loop to loop through generations 
-                               
-                                # add to the list by the formula to find 
-                                        # how many pairs will be in n 
-        pairs.append(abs(k*(i-1) + i-1))
- 
-    print(pairs)
-    print(sum(pairs))  # A way to total all the pairs in the list
-    
-    
 
 rabbitcounter(5,3)
-
 
 
